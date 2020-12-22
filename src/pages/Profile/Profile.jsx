5ad7ee9c4f7a85dca787e371d9984bbf9fe79ec1
Elwandy Tirtana Deriansyah <eltirder0@gmail.com>
2020-12-22 14:27:38 +0700

    fix navbar position and done profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import DetailProfile from '../../components/Profile/Profile'
 
 class Profile extends Component {
 	render () {
 		return (
 			<>
 				<Navbar />
+				<DetailProfile />
 			</>
 		)
 	}
