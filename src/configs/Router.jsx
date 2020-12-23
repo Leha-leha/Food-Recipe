fe42790ccd72adb62d8cc801c173560b6fcdf406
hendrasolih <hendra.solih.jp@gmail.com>
2020-12-23 22:55:28 +0700

    add private route for profile page

--- src/configs/Router.jsx
+++ src/configs/Router.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import { Provider } from "react-redux"
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import store from "../redux/store" 
- 
+import store from "../redux/store";
+
 import Homepage from "../pages/Homepage/Homepage";
-import Login from '../pages/auth/Index';
+import Login from "../pages/auth/Index";
 import Register from "../pages/auth/Register";
 import Forgot from "../pages/auth/Forgot";
-import Profile from '../pages/Profile/Profile'
+import Profile from "../pages/Profile/Profile";
 import Code from "../pages/auth/codeReset";
 import Reset from "../pages/auth/Reset";
 import Chat from "../pages/chat/Chat";
 import addRecipe from "../pages/recipes/addRecipe";
 
-const Router = () => {
-	return (
-		<Provider store={store}>
-		<BrowserRouter>
-			<Route exact path="/" component={Homepage} />
-			<Route path='/login' component={Login} />
-			<Route path="/register" component={Register} />
-			<Route path="/forgot" component={Forgot} />
-			<Route path="/code" component={Code} />
-			<Route path="/reset" component={Reset} />
-			<Route path='/profile' component={Profile} />
-			<Route path='/chat' component={Chat} />
-			<Route path='/addRecipe' component={addRecipe} />
-		</BrowserRouter>
-		</Provider>
-	)
-}
+import PrivateRoute from "../components/PrivateRoute";
 
-export default Router
+const Router = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/forgot" component={Forgot} />
+        <Route path="/code" component={Code} />
+        <Route path="/reset" component={Reset} />
+        <PrivateRoute path="/profile" component={Profile} />
+        <Route path="/chat" component={Chat} />
+        <Route path="/addRecipe" component={addRecipe} />
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+export default Router;
